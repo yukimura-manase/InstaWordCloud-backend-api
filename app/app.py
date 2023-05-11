0ffc6f120af5990f96eb95723f0074caf9305923
yukimura-manase <masahiro19951212@gmail.com>
2023-05-11 23:58:31 +0900

    Network作成・Routerの分離・ReadMe作成

--- app/app.py
+++ app/app.py
@@ -1,44 +1,23 @@
 # Flask-APIを構築するための基本機能
-from flask import Flask, Blueprint, render_template
-# 「.env」で設定した環境変数を使用する
-import settings
-# ログ出力のために、loggerをimportする
-from logoutput import logger
+from flask import Flask
 
-import traceback
+# CORSライブラリ
+from flask_cors import CORS
 
-# PythonからPythonScriptを呼び出すためのモジュール！ => プログラム内でコマンド実行！
-# import subprocess
+# ルーター: ルーティング設定
+import router
 
-# Flask-Appを立ち上げる & templateのDirを指定する
-app = Flask(__name__, template_folder='./templates')
+def create_app():
 
-# Generate Router Instance => Base_URLの設定はここ！
-router = Blueprint('router', __name__ )
+    # Flask-Appを立ち上げる & templateのDirを指定する
+    app = Flask(__name__, template_folder='./templates')
 
-# ルート
-@router.route('/')
-def index():
-    try :
-        logger.debug('Flask-API-ルート起動！')
-        return render_template('index.html')
-    except Exception as error :
-        error_msg:str = traceback.format_exc()
-        logger.error(f"APIルート: Error:  {error_msg}")
+    # Router を Flask-App に登録する && URL_末尾に /api を追加する
+    app.register_blueprint(router.router, url_prefix='/api')
 
+    # CORS許可でアプリを包む => APIへのCORS許可設定
+    CORS(app)
+    # resources={r"/api/*": {"origins": "http://localhost:8001"}}
+    return app
 
-# FrontAppの情報を確認するためのエンドポイント
-@router.route('/front_info')
-def front_info():
-    logger.debug('FrontApp-Info-アクセス！')
-    front_end_url = settings.FRONT_APP_URL
-    msg = f"FrontAppのURLは: {front_end_url}"
-    return msg
-
-
-# Router を Flask-App に登録する
-app.register_blueprint(
-    router, 
-    # 末尾に /api を追加する
-    url_prefix='/api'
-)
+app = create_app()
